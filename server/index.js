bdd868b014904d1f98fa3f46fd40c669500452fc
Yifan <yifanyemontpe@gmail.com>
2024-05-28 22:00:14 +0200

    Add index.html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.get('/', (_req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port), () => {
