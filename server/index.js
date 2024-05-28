b7b863f22495f96d6d4857d5ba2de26c79fd5ffc
Yifan <yifanyemontpe@gmail.com>
2024-05-28 22:40:57 +0200

    Implement socket connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import logger from 'morgan'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message)
+  })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (_req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port), () => {
+server.listen(port), () => {
   console.log(`Server running on port ${port}`)
 }
