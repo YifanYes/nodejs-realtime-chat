f66cb86f246df62dbeb8586cc622972febcea33b
Yifan <yifanyemontpe@gmail.com>
2024-05-29 10:30:46 +0200

    Recover messages from database and identify username

--- server/index.js
+++ server/index.js
@@ -15,24 +15,27 @@ const server = createServer(app, {
 })
 const io = new Server(server)
 const db = createClient({
-  url: "libsql://welcomed-blockbuster-yifanyes.turso.io",
+  url: 'libsql://welcomed-blockbuster-yifanyes.turso.io',
   authToken: process.env.DB_TOKEN
 })
 
 await db.execute(`
-  CREATE TABLE IF NOT EXISTS  MESSAGES (
+  CREATE TABLE IF NOT EXISTS MESSAGES (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    username TEXT
 )`)
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   socket.on('chat message', async (message) => {
     // Persisting message in database
     let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+
     try {
       result = await db.execute({
-        sql: `INSERT INTO messages (content) VALUES (:message)`,
-        args: { message }
+        sql: `INSERT INTO messages (content, username) VALUES (:message, :username)`,
+        args: { message, username }
       })
     } catch (error) {
       console.error(error)
@@ -40,8 +43,25 @@ io.on('connection', (socket) => {
     }
 
     // Broadcasting message to open connections
-    io.emit('chat message', message, result.lastInsertRowid.toString())
+    io.emit('chat message', message, result.lastInsertRowid.toString(), username)
   })
+
+  // In case client disconnected, recover messages
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: `SELECT id, content, username FROM messages WHERE id > ?`,
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.username)
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+  }
 })
 
 app.use(logger('dev'))
